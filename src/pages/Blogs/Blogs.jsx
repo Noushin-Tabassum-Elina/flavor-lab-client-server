052a5cae757a1c9917ddddc1162a3daf642df671
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-05-04 23:35:38 +0600

    blogs added

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -3,8 +3,10 @@ import BlogPost from '../BlogPost/BlogPost';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 
 const Blogs = () => {
+
     return (
         <div className='mt-18 p-8 mb-10'>
+            
             <div className='flex flex-col items-center justify-between lg:px-4 py-8 bg-slate-400 md:flex-row md:justify-between lg:py-16'>
                 <div className='m-16'>
                 <h1 className='text-4xl font-bold'>Blogs</h1> 
