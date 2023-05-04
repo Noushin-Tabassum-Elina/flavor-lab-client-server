2a25ddfaa9073865991013c62406f5c74386e98e
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-05-04 23:26:47 +0600

    navbar added

--- src/components/Shared/NavigationBar/NavigationBar.jsx
+++ src/components/Shared/NavigationBar/NavigationBar.jsx
@@ -12,12 +12,14 @@ const NavigationBar = () => {
     }
 
     return (
+        
         <div className="navbar bg-slate-800 " style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1 }}>
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="bg-white h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
+
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ">
                         <li><NavLink className={({ isActive }) =>
                             isActive ? 'bg-green-500 font-bold' : 'bg-red-500 font-thin'
